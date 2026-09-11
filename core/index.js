39f131c66511c4651539ffe22dfd52656d1c29a4
ran - Femboy <ranzxx62@gmail.com>
2026-09-11 13:02:51 +0700

    Create index.js

---
+++ core/index.js
@@ -0,0 +1,13 @@
+const Client = require("./classes/client");
+const CommandHandler = require("./classes/command-handler");
+const Config = require("./classes/config");
+const Cooldown = require("./classes/cooldown");
+const HotReload = require("./classes/hot-reload");
+
+module.exports = {
+    Client,
+    CommandHandler,
+    Config,
+    Cooldown,
+    HotReload
+};
